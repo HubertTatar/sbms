b8314d1ddc275cee3e74e6cd157f6a2ed75ba991
Hubert <hutatar@gmail.com>
2017-07-14 23:08:36 +0200

    commit

--- users/src/main/java/com/hutatar/UsersApplication.java
+++ users/src/main/java/com/hutatar/UsersApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //https://fernandoabcampos.wordpress.com/
 //http://www.dineshonjava.com/2017/01/microservices-with-spring-boot.html
 //https://spring.io/blog/2015/07/14/microservices-with-spring
+//https://github.com/cer/event-sourcing-examples
+//http://localhost:8001/users/dev
 @SpringBootApplication
 public class UsersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
-		Test t = new Test();
-		System.out.println("####################################"+t.test);
 	}
 }
