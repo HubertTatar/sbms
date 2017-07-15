9f0ae8457f4208c66c529635b4936413f4d1d00a
Hubert <hutatar@gmail.com>
2017-07-15 18:28:17 +0200

    commit

--- users/src/main/java/com/hutatar/Test.java
+++ users/src/main/java/com/hutatar/Test.java
@@ -1,11 +1,13 @@
 package com.hutatar;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class Test {
     @Value("${lucky-word}")
     public String test;
@@ -13,5 +15,13 @@ public class Test {
     @GetMapping("/word")
     public String get(){
         return test;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
     }
 }
